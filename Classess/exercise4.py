61faf68e5908dd45e824270b23d4b64525f5c544
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-02-04 20:55:26 +0600

    Add files via upload

--- Classess/exercise4.py
+++ Classess/exercise4.py
@@ -2,24 +2,31 @@ class Point:
     def __init__(self,x,y):
         self.x=x
         self.y=y
-        print(self.x,self.y)
-
+        
     def show(self):
-        print(self.x-5,self.y-5)
+         print(self.x,self.y)
 
     def move(self):
         self.x=self.x-5
-        self.x=self.x-5
+        self.y=self.y-5
         print(self.x,self.y)
 
     def dist(self):
-        if self.x>self.y:
-            print(self.x-self.y)
-        elif self.x==self.y:
-            print(0)
+        self.point2x=3
+        self.point2y=7
+        self.distance_x=self.point2x-self.x
+        self.distance_y=self.point2y-self.y
+        if self.distance_x>0 and self.distance_y>0:
+            print(self.distance_x,self.distance_y)
         else:
-            print(self.y-self.x)
+            if self.distnace_x>0:
+                print(self.distnace_y*-1)
+            elif self.distance_x<0 and self.distance_y<0:
+                print(self.distance_x*-1,self.distance_y*-1)
 
+            else:
+                print(self.distnace_y*-1)
+           
 
 x=int(input())
 y=int(input())
